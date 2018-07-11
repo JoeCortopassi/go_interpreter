edeb8e7d8b01e3150092773e934abe1fcb8c00ab
Joe Cortopassi <joe@joecortopassi.com>
2018-07-11 08:09:30 -0500

    add additional operators and test coverage to lexer

--- token/token.go
+++ token/token.go
@@ -14,8 +14,14 @@ const (
 	IDENT = "IDENT" // add, foobar, x, y...
 	INT   = "INT"   // 1,2,3...
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
 	//Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
